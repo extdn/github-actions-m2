4fcd3c0586bce4363b2dce52fd407c0a03db7153
Kristof Ringleff, Fooman <kristof@fooman.co.nz>
2019-11-22 16:28:55 +1300

    correct repo syntax

--- install-m2-from-mirror/dist/index.js
+++ install-m2-from-mirror/dist/index.js
@@ -69,10 +69,8 @@ async function run() {
     const options = {};
  	await exec.exec(`composer create-project --repository=https://repo-magento-mirror.fooman.co.nz/ magento/project-community-edition:${ceversion} m2-folder --no-install --no-interaction`);
 	options.cwd = './m2-folder';
-	await exec.exec('ls');
-	await exec.exec('ls', [], options);
 	await exec.exec('composer', ['config', '--unset', 'repo.0'], options);
-	await exec.exec('composer', ['config', 'repo', 'repo.foomanmirror', 'composer', 'https://repo-magento-mirror.fooman.co.nz/'], options);
+	await exec.exec('composer', ['config', 'repo.foomanmirror', 'composer', 'https://repo-magento-mirror.fooman.co.nz/'], options);
 	await exec.exec('composer', ['install', ' --prefer-dist'], options);
   } 
   catch (error) {
