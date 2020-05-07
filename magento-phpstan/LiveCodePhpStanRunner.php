9cb5403ef2e61dfb188e9601690e7edd9297e5da
Kristof Ringleff, Fooman <kristof@fooman.co.nz>
2020-05-07 17:03:37 +1200

    Start work on a phpstan action

---
+++ magento-phpstan/LiveCodePhpStanRunner.php
@@ -0,0 +1,114 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+declare(strict_types=1);
+
+namespace Magento\TestFramework\CodingStandard\Tool;
+
+use Magento\TestFramework\CodingStandard\ToolInterface;
+
+/**
+ * PhpStan tool wrapper.
+ */
+class LiveCodePhpStanRunner implements ToolInterface
+{
+    /**
+     * Rule level to be used.
+     *
+     * @see https://github.com/phpstan/phpstan#rule-levels
+     */
+    private const RULE_LEVEL = 0;
+
+    /**
+     * Memory limit required by PHPStan for full Magento project scan.
+     */
+    private const MEMORY_LIMIT = '4G';
+
+    /**
+     * Error formatter to be used.
+     *
+     * @see https://github.com/phpstan/phpstan#existing-error-formatters-to-be-used
+     */
+    private const ERROR_FORMAT = 'raw';
+
+    /**
+     * Report file.
+     *
+     * @var string
+     */
+    private $reportFile;
+
+    /**
+     * PHPStan configuration file in neon format.
+     *
+     * @var string
+     */
+    private $confFile;
+
+    /**
+     * @param string $confFile
+     * @param string $reportFile
+     */
+    public function __construct(string $confFile, string $reportFile)
+    {
+        $this->reportFile = $reportFile;
+        $this->confFile = $confFile;
+    }
+
+    /**
+     * @inheritdoc
+     * @SuppressWarnings(PHPMD.UnusedLocalVariable)
+     */
+    public function canRun(): bool
+    {
+        // phpcs:disable Magento2.Security.InsecureFunction
+        exec($this->getCommand() . ' --version', $output, $exitCode);
+        return $exitCode === 0;
+    }
+
+    /**
+     * @inheritdoc
+     * @SuppressWarnings(PHPMD.UnusedLocalVariable)
+     */
+    public function run(array $whiteList): int
+    {
+
+        $command = $this->getCommand() . ' analyse' .
+            ' --level ' . self::RULE_LEVEL .
+            ' --no-progress' .
+            ' --error-format=' . self::ERROR_FORMAT .
+            ' --memory-limit=' . self::MEMORY_LIMIT .
+            // phpcs:ignore Magento2.Functions.DiscouragedFunction
+            ' --configuration ' . escapeshellarg($this->confFile) .
+            ' ' . $this->getSourceCodePath($whiteList).
+            ' > ' . $this->reportFile;
+
+        // phpcs:disable Magento2.Security.InsecureFunction
+        exec($command, $output, $exitCode);
+
+        return $exitCode;
+    }
+
+    /**
+     * Get PHPStan CLI command
+     *
+     * @return string
+     */
+    private function getCommand(): string
+    {
+        // phpcs:ignore Magento2.Security.IncludeFile
+        $vendorDir = require BP . '/app/etc/vendor_path.php';
+        return 'php ' . BP . '/' . $vendorDir . '/bin/phpstan';
+    }
+
+    private function getSourceCodePath($whiteList): string
+    {
+        if(!empty($whiteList)){
+            return implode(',', $whiteList);
+        }
+        return defined('PATH_TO_SOURCE')? PATH_TO_SOURCE : '/var/www/html';
+    }
+
+}
---
+++ magento-phpstan/PhpStanRunner.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+declare(strict_types=1);
+
+namespace Magento\Test\Php;
+
+use Magento\Framework\App\Utility\Files;
+use Magento\TestFramework\CodingStandard\Tool\LiveCodePhpStanRunner;
+
+/**
+ * Set of tests for static code analysis, e.g. code style, code complexity, copy paste detecting, etc.
+ */
+class PhpStanRunner extends \PHPUnit\Framework\TestCase
+{
+    /**
+     * @var string
+     */
+    protected static $reportDir = '';
+
+    /**
+     * @var string
+     */
+    protected static $pathToSource = '';
+
+    /**
+     * Setup basics for all tests
+     *
+     * @return void
+     */
+    public static function setUpBeforeClass()
+    {
+        self::$pathToSource = BP;
+        self::$reportDir = self::$pathToSource . '/dev/tests/static/report';
+        if (!is_dir(self::$reportDir)) {
+            mkdir(self::$reportDir);
+        }
+    }
+
+    public function testPhpStan()
+    {
+        $reportFile = self::$reportDir . '/phpstan_report.txt';
+        $confFile = __DIR__ . '/_files/phpstan/phpstan.neon';
+
+        if (!file_exists($reportFile)) {
+            touch($reportFile);
+        }
+
+        $phpStan = new LiveCodePhpStanRunner($confFile, $reportFile);
+        $exitCode = $phpStan->run([]);
+        $report = file_get_contents($reportFile);
+
+        $errorMessage = empty($report) ?
+            'PHPStan command run failed.' : 'PHPStan detected violation(s):' . PHP_EOL . $report;
+        $this->assertEquals(0, $exitCode, $errorMessage);
+
+        // delete empty reports
+        if (file_exists($reportFile)) {
+            unlink($reportFile);
+        }
+    }
+
+}
+
