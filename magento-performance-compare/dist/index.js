2273882a43948742a30e06817be3aacd137a89ed
Kristof Ringleff, Fooman <kristof@fooman.co.nz>
2020-05-12 18:53:27 +1200

    either comment on commit or pr

--- magento-performance-compare/dist/index.js
+++ magento-performance-compare/dist/index.js
@@ -2000,7 +2000,7 @@ async function run() {
         if (!fs.existsSync(baselineFileName) || !fs.existsSync(afterFileName)) {
             throw new Error("Can't find blackfire profiles to compare");
         }
-console.log(fs.readFileSync(baselineFileName).toString());
+
         let baseline = JSON.parse(fs.readFileSync(baselineFileName).toString());
         let after = JSON.parse(fs.readFileSync(afterFileName).toString());
 
@@ -2008,14 +2008,25 @@ console.log(fs.readFileSync(baselineFileName).toString());
         let memoryDiff = Number(((baseline.envelope.pmu - after.envelope.pmu) / baseline.envelope.wt) * 100).toFixed(2);
         let sqlDiff = Number(((baseline.arguments["io.db.query"]["*"].ct - after.arguments["io.db.query"]["*"].ct) / baseline.arguments["io.db.query"]["*"].ct) * 100).toFixed(2);
 
-        core.debug("Time Difference " + timeDiff + "Memory Difference " + memoryDiff + "Number of SQL Queries " + sqlDiff + "Profile" + baseline._links.graph_url.href);
         const github = new GitHub(token);
 
-        const new_comment = github.commits.createCommitComment({
-            ...context.repo,
-            commit_sha: process.env.GITHUB_SHA,
-            body: "Time Difference " + timeDiff + "Memory Difference " + memoryDiff + "Number of SQL Queries " + sqlDiff + "Profile" + baseline._links.graph_url.href
-        });
+        const message = "Time Difference " + timeDiff + "%\nMemory Difference " + memoryDiff + "%\nNumber of SQL Queries " + sqlDiff + "%\n\n(Profile)[" + baseline._links.graph_url.href + "]";
+        core.debug(message);
+        if (context.payload.pull_request == null) {
+            const new_comment = github.createCommitComment({
+                ...context.repo,
+                commit_sha: process.env.GITHUB_SHA,
+                body: message
+            });
+        } else {
+            const pull_request_number = context.payload.pull_request.number;
+            const new_comment = github.issues.createComment({
+                ...context.repo,
+                issue_number: pull_request_number,
+                body: message
+            });
+        }
+
         if (timeDiff > threshold || memoryDiff > threshold || sqlDiff > threshold) {
             core.setFailed("Performance decreased more than configured threshold.");
         }
