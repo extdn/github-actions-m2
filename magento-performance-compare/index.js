e2a7d4f4a7a6dde0b5e1327d89e98bc4f3b81513
Kristof Ringleff, Fooman <kristof@fooman.co.nz>
2020-05-12 13:55:51 +1200

    Provide result as comment

--- magento-performance-compare/index.js
+++ magento-performance-compare/index.js
@@ -1,27 +1,38 @@
 const core = require('@actions/core');
-const exec = require('@actions/exec');
-const github = require("@actions/github");
-
+const {context, GitHub} = require("@actions/github");
 const fs = require('fs');
 
 async function run() {
-try {
+    try {
 
-    const threshold = core.getInput('threshold');
-    const baselineFileName = core.getInput('baseline-file');
-    const afterFileName = core.getInput('after-file');
+        const token = core.getInput('github-token', {required: true})
+        const threshold = core.getInput('threshold');
+        const baselineFileName = core.getInput('baseline-file');
+        const afterFileName = core.getInput('after-file');
 
-    if(!fs.existsSync(process.env.GITHUB_WORKSPACE + baselineFileName) && !fs.existsSync(process.env.GITHUB_WORKSPACE + afterFileName)) {
-        core.setFailed("Can't find blackfire profiles to compare");
-    }
-    let baseline = JSON.parse(fs.readFileSync(process.env.GITHUB_WORKSPACE + baselineFileName));
-    console.log(baseline._links.graph_url.href);
-    let after = JSON.parse(fs.readFileSync(process.env.GITHUB_WORKSPACE + afterFileName));
-    console.log("Time Difference" + Number(((baseline.envelope.wt-after.envelope.wt)/baseline.envelope.wt)*100).toFixed(2));
-    console.log("Memory Difference" + Number(((baseline.envelope.pmu-after.envelope.pmu)/baseline.envelope.wt)*100).toFixed(2).toFixed(2));
-    console.log("Number of SQL Queries" + Number(((baseline["io.db.query"]["*"].ct-after["io.db.query"]["*"].ct)/baseline["io.db.query"]["*"].ct)*100).toFixed(2));
-}
-    catch (error) {
+        if (!fs.existsSync(process.env.GITHUB_WORKSPACE + baselineFileName) && !fs.existsSync(process.env.GITHUB_WORKSPACE + afterFileName)) {
+            core.setFailed("Can't find blackfire profiles to compare");
+        }
+
+        let baseline = JSON.parse(fs.readFileSync(process.env.GITHUB_WORKSPACE + baselineFileName));
+        let after = JSON.parse(fs.readFileSync(process.env.GITHUB_WORKSPACE + afterFileName));
+
+        let timeDiff = Number(((baseline.envelope.wt - after.envelope.wt) / baseline.envelope.wt) * 100).toFixed(2);
+        let memoryDiff = Number(((baseline.envelope.pmu - after.envelope.pmu) / baseline.envelope.wt) * 100).toFixed(2).toFixed(2);
+        let sqlDiff = Number(((baseline["io.db.query"]["*"].ct - after["io.db.query"]["*"].ct) / baseline["io.db.query"]["*"].ct) * 100).toFixed(2);
+
+        const github = new GitHub(token);
+
+        const new_comment = github.commits.createComment({
+            ...context.repo,
+            sha: process.env.GITHUB_SHA,
+            body: "Time Difference " + timeDiff + "Memory Difference " + memoryDiff + "Number of SQL Queries " + sqlDiff + "Profile" + baseline._links.graph_url.href
+        });
+        if (timeDiff > threshold || memoryDiff > threshold || sqlDiff > threshold) {
+            core.setFailed("Performance decreased more than configured threshold.");
+        }
+
+    } catch (error) {
         core.setFailed(error.message);
     }
 }
