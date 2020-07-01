f6928b605520b4a2f58e949c08eb9dccd5feadf9
jisse Reitsma <jisse@yireo.com>
2020-07-01 10:22:21 +0200

    Add Integration Tests

--- magento-integration-tests/entrypoint.sh
+++ magento-integration-tests/entrypoint.sh
@@ -2,27 +2,33 @@
 
 set -e
 
+test -z "${INPUT_EXTENSION_VENDOR}" && (echo "'extension_vendor' is not set" && exit)
+test -z "${INPUT_EXTENSION_MODULE}" && (echo "'extension_module' is not set" && exit)
 test -z "${INPUT_CE_VERSION}" && (echo "'ce_version' is not set" && exit)
 test -z "${INPUT_MARKETPLACE_URL}" && (echo "'marketplace_url' is not set" && exit)
 
-TMP_PATH=/tmp/m2
+MAGENTO_ROOT=/tmp/m2
 PROJECT_PATH=$GITHUB_WORKSPACE
 MARKETPLACE_URL=$INPUT_MARKETPLACE_URL
 CE_VERSION=$INPUT_CE_VERSION
 
 # Magento installation
-composer create-project --repository=$MARKETPLACE_URL magento/project-community-edition:${CE_VERSION} $TMP_PATH --no-install --no-interaction
-cd $TMP_PATH
+composer create-project --repository=$MARKETPLACE_URL magento/project-community-edition:${CE_VERSION} $MAGENTO_ROOT --no-install --no-interaction
+cd $MAGENTO_ROOT
 composer config --unset repo.0
 composer config repo.custom-mirror composer $MARKETPLACE_URL
 composer install --prefer-dist
 
 # Setup extension
+mkdir app/code/$INPUT_EXTENSION_VENDOR
+cd app/code/$INPUT_EXTENSION_VENDOR
+ln -s $GITHUB_WORKSPACE $INPUT_EXTENSION_MODULE
 
 # Prepare for integration tests
+cd $MAGENTO_ROOT
 cp /install-config-mysql.php dev/tests/integration/etc/install-config-mysql.php
 cp /phpunit.xml dev/tests/integration/phpunit.xml
 
 # Run the integration tests
-cd dev/tests/integration && ../../../vendor/bin/phpunit -c phpunit.xml
+cd $MAGENTO_ROOT/dev/tests/integration && ../../../vendor/bin/phpunit -c phpunit.xml
 
