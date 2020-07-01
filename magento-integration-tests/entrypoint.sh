02d71f5352b26a0a04bb83fa0d8019cf5438e5f7
jisse Reitsma <jisse@yireo.com>
2020-07-01 12:09:08 +0200

    somebody keeps erasing my changes.

--- magento-integration-tests/entrypoint.sh
+++ magento-integration-tests/entrypoint.sh
@@ -2,25 +2,24 @@
 
 set -e
 
-test -z "${INPUT_EXTENSION_VENDOR}" && (echo "'extension_vendor' is not set" && exit)
-test -z "${INPUT_EXTENSION_MODULE}" && (echo "'extension_module' is not set" && exit)
-test -z "${INPUT_CE_VERSION}" && (echo "'ce_version' is not set" && exit)
-test -z "${INPUT_MARKETPLACE_URL}" && (echo "'marketplace_url' is not set" && exit)
+test -z "${INPUT_EXTENSION_VENDOR}" && (echo "'extension_vendor' is not set in your GitHub Actions YAML file" && exit)
+test -z "${INPUT_EXTENSION_MODULE}" && (echo "'extension_module' is not set in your GitHub Actions YAML file" && exit)
+test -z "${INPUT_CE_VERSION}" && (echo "'ce_version' is not set in your GitHub Actions YAML file" && exit)
+test -z "${MAGENTO_MARKETPLACE_USERNAME}" && (echo "'MAGENTO_MARKETPLACE_USERNAME' is not available as a secret" && exit)
+test -z "${MAGENTO_MARKETPLACE_PASSWORD}" && (echo "'MAGENTO_MARKETPLACE_PASSWORD' is not available as a secret" && exit)
 
 MAGENTO_ROOT=/tmp/m2
 PROJECT_PATH=$GITHUB_WORKSPACE
-MARKETPLACE_URL=$INPUT_MARKETPLACE_URL
 CE_VERSION=$INPUT_CE_VERSION
 
+# Magento credentials
+composer global config http-basic.repo.magento.com username $MAGENTO_MARKETPLACE_USERNAME
+composer global config http-basic.repo.magento.com password $MAGENTO_MARKETPLACE_PASSWORD
+
 # Magento installation
-echo "Using Marketpace URL $MARKETPLACE_URL"
 composer global require hirak/prestissimo
-composer create-project --repository=$MARKETPLACE_URL magento/project-community-edition:${CE_VERSION} $MAGENTO_ROOT --no-install --no-interaction
+composer create-project --repository=https://marketplace.magento.com/ magento/project-community-edition:${CE_VERSION} $MAGENTO_ROOT --no-install --no-interaction
 cd $MAGENTO_ROOT
-composer config --unset repo.0
-composer config repo.custom-mirror composer $MARKETPLACE_URL
-cat composer.json
-exit
 composer install --prefer-dist
 
 # Setup extension
