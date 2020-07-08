00a88f177d0cccb4dc03cf4db213f27d3926ada8
jisse Reitsma <jisse@yireo.com>
2020-07-08 15:30:12 +0200

    Convert variables into environment vars

--- magento-unit-tests/entrypoint.sh
+++ magento-unit-tests/entrypoint.sh
@@ -2,15 +2,14 @@
 
 set -e
 
-test -z "${INPUT_MODULE_NAME}" && (echo "'module_name' is not set in your GitHub Actions YAML file" && exit 1)
-test -z "${INPUT_COMPOSER_NAME}" && (echo "'composer_name' is not set in your GitHub Actions YAML file" && exit 1)
-test -z "${INPUT_CE_VERSION}" && (echo "'ce_version' is not set in your GitHub Actions YAML file" && exit 1)
-test -z "${MAGENTO_MARKETPLACE_USERNAME}" && (echo "'MAGENTO_MARKETPLACE_USERNAME' is not available as a secret" && exit 1)
-test -z "${MAGENTO_MARKETPLACE_PASSWORD}" && (echo "'MAGENTO_MARKETPLACE_PASSWORD' is not available as a secret" && exit 1)
+test -z "${MODULE_NAME}" && (echo "'module_name' is not set" && exit 1)
+test -z "${COMPOSER_NAME}" && (echo "'composer_name' is not set" && exit 1)
+test -z "${CE_VERSION}" && (echo "'ce_version' is not set" && exit 1)
+test -z "${MAGENTO_MARKETPLACE_USERNAME}" && (echo "'MAGENTO_MARKETPLACE_USERNAME' is not set" && exit 1)
+test -z "${MAGENTO_MARKETPLACE_PASSWORD}" && (echo "'MAGENTO_MARKETPLACE_PASSWORD' is not set" && exit 1)
 
 MAGENTO_ROOT=/tmp/m2
 PROJECT_PATH=$GITHUB_WORKSPACE
-CE_VERSION=$INPUT_CE_VERSION
 
 echo "Setup Magento credentials"
 composer global config http-basic.repo.magento.com $MAGENTO_MARKETPLACE_USERNAME $MAGENTO_MARKETPLACE_PASSWORD
@@ -23,12 +22,12 @@ echo "Setup extension source folder within Magento root"
 cd $MAGENTO_ROOT
 mkdir -p local-source/
 cd local-source/
-cp -R ${GITHUB_WORKSPACE}/${INPUT_MODULE_SOURCE} $INPUT_MODULE_NAME
+cp -R ${GITHUB_WORKSPACE}/${MODULE_SOURCE} $MODULE_NAME
 
 echo "Configure extension source in composer"
 cd $MAGENTO_ROOT
 composer config repositories.local-source path local-source/\*
-composer require $INPUT_COMPOSER_NAME:@dev --no-update --no-interaction
+composer require $COMPOSER_NAME:@dev --no-update --no-interaction
 
 echo "Run installation"
 composer install --prefer-dist --no-interaction --no-progress --no-suggest
@@ -41,7 +40,7 @@ fi
 echo "Using PHPUnit file: $INPUT_PHPUNIT_FILE"
 echo "Prepare for unit tests"
 cd $MAGENTO_ROOT
-sed "s#%COMPOSER_NAME%#$INPUT_COMPOSER_NAME#g" $INPUT_PHPUNIT_FILE > dev/tests/unit/phpunit.xml
+sed "s#%COMPOSER_NAME%#$COMPOSER_NAME#g" $INPUT_PHPUNIT_FILE > dev/tests/unit/phpunit.xml
 
 echo "Run the unit tests"
 cd $MAGENTO_ROOT/dev/tests/unit && ../../../vendor/bin/phpunit -c phpunit.xml
