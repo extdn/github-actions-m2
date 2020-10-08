850707dbbafeffdb8cb4d7082f425c4061410aff
Kristof Ringleff, Fooman <kristof@fooman.co.nz>
2020-10-08 14:52:42 +1300

    The problem matcher needs to be on the host

--- magento-coding-standard/entrypoint.sh
+++ magento-coding-standard/entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/sh -l
 
-echo "::add-matcher::/problem-matcher.json"
+# Copy the matcher to a shared volume with the host; otherwise "add-matcher"
+# can't find it.
+cp /problem-matcher.json ${HOME}/
+echo "::add-matcher::${HOME}/problem-matcher.json"
 
 sh -c "/root/.composer/vendor/bin/phpcs --standard=Magento2 $GITHUB_WORKSPACE -s $*"
