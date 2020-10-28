3220dc76bb5ddb201602d2885dc30aac94270e79
Kristof Ringleff, Fooman <kristof@fooman.co.nz>
2020-10-28 20:37:44 +1300

    set environment variable on phpunit command

--- magento-mess-detector/entrypoint.sh
+++ magento-mess-detector/entrypoint.sh
@@ -1,2 +1,3 @@
 #!/bin/sh -l
-PATH_TO_SOURCE=$GITHUB_WORKSPACE sh -c "cd /m2/dev/tests/static && /m2/vendor/bin/phpunit -c /m2/dev/tests/static/phpunit.phpmd.xml $*"
+echo "Testing Code in "$GITHUB_WORKSPACE
+sh -c "cd /m2/dev/tests/static && PATH_TO_SOURCE=$GITHUB_WORKSPACE /m2/vendor/bin/phpunit -c /m2/dev/tests/static/phpunit.phpmd.xml $*"
