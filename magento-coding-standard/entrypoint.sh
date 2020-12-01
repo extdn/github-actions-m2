3d1534d90d842b138f8dcb67d46c8b612eb356d0
jisse Reitsma <jisse@yireo.com>
2020-12-01 17:58:26 +0100

    Fix input of custom PHPCS file

--- magento-coding-standard/entrypoint.sh
+++ magento-coding-standard/entrypoint.sh
@@ -6,5 +6,5 @@ cp /problem-matcher.json ${HOME}/
 echo "::add-matcher::${HOME}/problem-matcher.json"
 
 cd $GITHUB_WORKSPACE
-test -z "${PHPCS_STANDARD}" && PHPCS_STANDARD=Magento2
-sh -c "/root/.composer/vendor/bin/phpcs --report=checkstyle --standard=$PHPCS_STANDARD $GITHUB_WORKSPACE -s $*"
+test -z "${INPUT_PHPCS_STANDARD}" && INPUT_PHPCS_STANDARD=Magento2
+sh -c "/root/.composer/vendor/bin/phpcs --report=checkstyle --standard=$INPUT_PHPCS_STANDARD $GITHUB_WORKSPACE -s $*"
