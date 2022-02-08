50b4dea799db6246d8cd23177fa83fe4b7905f33
Jisse Reitsma <jisse@yireo.com>
2022-02-08 14:12:09 +0100

    Add PHPCS configuration for severity and report

--- magento-coding-standard/entrypoint.sh
+++ magento-coding-standard/entrypoint.sh
@@ -6,5 +6,9 @@ cp /problem-matcher.json ${HOME}/
 echo "::add-matcher::${HOME}/problem-matcher.json"
 
 cd $GITHUB_WORKSPACE
+
 test -z "${INPUT_PHPCS_STANDARD}" && INPUT_PHPCS_STANDARD=Magento2
-sh -c "/root/.composer/vendor/bin/phpcs --report=checkstyle --standard=$INPUT_PHPCS_STANDARD $GITHUB_WORKSPACE -s $*"
+test -z "${INPUT_PHPCS_SEVERITY}" && INPUT_PHPCS_SEVERITY=8
+test -z "${INPUT_PHPCS_REPORT}" && INPUT_PHPCS_REPORT=checkstyle
+
+sh -c "/root/.composer/vendor/bin/phpcs --report=${INPUT_PHPCS_REPORT} --severity=${INPUT_PHPCS_SEVERITY} --standard=$INPUT_PHPCS_STANDARD $GITHUB_WORKSPACE -s $*"
