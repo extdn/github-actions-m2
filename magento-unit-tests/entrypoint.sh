9872bebae1a029f900311bdd76b4b2f8952fd3e5
jisse Reitsma <jisse@yireo.com>
2022-02-10 15:04:43 +0100

    Beautify entrypoint unit tests

--- magento-unit-tests/entrypoint.sh
+++ magento-unit-tests/entrypoint.sh
@@ -3,14 +3,22 @@
 set -e
 
 test -z "${CE_VERSION}" || MAGENTO_VERSION=$CE_VERSION
+test -z "${PROJECT_NAME}" || MAGENTO_PROJECT_NAME=$PROJECT_NAME
+test -z "${MAGENTO_PROJECT_NAME}" || MAGENTO_PROJECT_NAME=$INPUT_PROJECT_NAME
 
 test -z "${MODULE_NAME}" && MODULE_NAME=$INPUT_MODULE_NAME
 test -z "${COMPOSER_NAME}" && COMPOSER_NAME=$INPUT_COMPOSER_NAME
 test -z "${MAGENTO_VERSION}" && MAGENTO_VERSION=$INPUT_MAGENTO_VERSION
 
+test -z "$MAGENTO_VERSION" && MAGENTO_VERSION="2.4.3-p1"
+test -z "$COMPOSER_VERSION" && test "$MAGENTO_VERSION" =~ ^2.4.* && COMPOSER_VERSION=2
+test -z "$COMPOSER_VERSION" && COMPOSER_VERSION=1
+test -z "$MAGENTO_PROJECT_NAME" && MAGENTO_PROJECT_NAME="magento/project-community-edition"
+
 test -z "${MODULE_NAME}" && (echo "'module_name' is not set" && exit 1)
 test -z "${COMPOSER_NAME}" && (echo "'composer_name' is not set" && exit 1)
 test -z "${MAGENTO_VERSION}" && (echo "'magento_version' is not set" && exit 1)
+test -z "${MAGENTO_PROJECT_NAME}" && (echo "'project_name' is not set" && exit 1)
 
 MAGENTO_ROOT=/tmp/m2
 PROJECT_PATH=$GITHUB_WORKSPACE
@@ -20,7 +28,7 @@ echo "Setup Magento credentials"
 test -z "${MAGENTO_MARKETPLACE_USERNAME}" || composer global config http-basic.repo.magento.com $MAGENTO_MARKETPLACE_USERNAME $MAGENTO_MARKETPLACE_PASSWORD
 
 echo "Prepare composer installation"
-composer create-project --repository=$REPOSITORY_URL magento/project-community-edition:${MAGENTO_VERSION} $MAGENTO_ROOT --no-install --no-interaction --no-progress
+composer create-project --repository=$REPOSITORY_URL ${MAGENTO_PROJECT_NAME}:${MAGENTO_VERSION} $MAGENTO_ROOT --no-install --no-interaction --no-progress
 
 echo "Setup extension source folder within Magento root"
 cd $MAGENTO_ROOT
@@ -33,6 +41,7 @@ cd $MAGENTO_ROOT
 composer config repositories.local-source path local-source/\*
 composer require $COMPOSER_NAME:@dev --no-update --no-interaction
 
+echo "Pre Install Script [magento_pre_install_script]: $INPUT_MAGENTO_PRE_INSTALL_SCRIPT"
 if [[ ! -z "$INPUT_MAGENTO_PRE_INSTALL_SCRIPT" && -f "${GITHUB_WORKSPACE}/$INPUT_MAGENTO_PRE_INSTALL_SCRIPT" ]] ; then
     echo "Running custom pre-installation script: ${INPUT_MAGENTO_PRE_INSTALL_SCRIPT}"
     . ${GITHUB_WORKSPACE}/$INPUT_MAGENTO_PRE_INSTALL_SCRIPT
