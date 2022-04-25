349f3028bf9aa3d165aa3f84b30b6144a64b1a87
Jisse Reitsma <jisse@yireo.com>
2022-04-25 19:43:00 +0200

    Add file extensions

--- magento-coding-standard/entrypoint.sh
+++ magento-coding-standard/entrypoint.sh
@@ -10,13 +10,21 @@ cd $GITHUB_WORKSPACE
 test -z "${PHPCS_STANDARD}" && PHPCS_STANDARD=$INPUT_PHPCS_STANDARD
 test -z "${PHPCS_SEVERITY}" && PHPCS_SEVERITY=$INPUT_PHPCS_SEVERITY
 test -z "${PHPCS_REPORT}" && PHPCS_REPORT=$INPUT_PHPCS_REPORT
+test -z "${PHPCS_EXTENSIONS}" && PHPCS_EXTENSIONS=$INPUT_PHPCS_EXTENSIONS
 
 test -z "${PHPCS_STANDARD}" && PHPCS_STANDARD=Magento2
 test -z "${PHPCS_SEVERITY}" && PHPCS_SEVERITY=8
 test -z "${PHPCS_REPORT}" && PHPCS_REPORT=checkstyle
+test -z "${PHPCS_EXTENSIONS}" && PHPCS_EXTENSIONS=php
 
 echo "PHPCS report: ${PHPCS_REPORT}"
 echo "PHPCS standard: ${PHPCS_STANDARD}"
 echo "PHPCS severity: ${PHPCS_SEVERITY}"
+echo "PHPCS severity: ${PHPCS_EXTENSIONS}"
 
-sh -c "/root/.composer/vendor/bin/phpcs --report=${PHPCS_REPORT} --severity=${PHPCS_SEVERITY} --standard=$PHPCS_STANDARD $GITHUB_WORKSPACE -s $*"
+sh -c "/root/.composer/vendor/bin/phpcs \
+  --report=${PHPCS_REPORT} \
+  --extensions=${PHPCS_EXTENSIONS} \
+  --severity=${PHPCS_SEVERITY} \
+  --standard=$PHPCS_STANDARD $GITHUB_WORKSPACE \
+  -s $*"
