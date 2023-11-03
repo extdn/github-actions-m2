02b2ca0ac49be3cbaff487e473ecfe702dd3a286
Dan Wallis <dan@wallis.nz>
2023-11-03 17:15:15 +0000

    Add support for PHP 8.2

--- magento-unit-tests/build-and-push.sh
+++ magento-unit-tests/build-and-push.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-for tag in 7.2 7.3 7.4 8.1; do
+for tag in 7.2 7.3 7.4 8.1 8.2; do
     docker build -t yireo/github-actions-magento-unit-tests:$tag -f Dockerfile:$tag .
     docker push yireo/github-actions-magento-unit-tests:$tag
 done
