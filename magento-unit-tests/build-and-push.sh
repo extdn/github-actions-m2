1470acfe6b366793ad7ee9f600cf79f938295608
Dan Wallis <dan@wallis.nz>
2025-04-11 19:57:56 +0100

    Add support for PHP 8.4 / Magento 2.4.8

--- magento-unit-tests/build-and-push.sh
+++ magento-unit-tests/build-and-push.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-for tag in 7.2 7.3 7.4 8.1 8.2 8.3; do
+for tag in 7.2 7.3 7.4 8.1 8.2 8.3 8.4; do
     docker build -t yireo/github-actions-magento-unit-tests:$tag -f Dockerfile:$tag .
     docker push yireo/github-actions-magento-unit-tests:$tag
 done
